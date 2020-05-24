433f08a08efcaaf22533660b5df3fa60d2e92cda
doblepapi <jasonbnick@gmial.com>
2020-05-24 19:03:13 +0200

    create the bbdd

--- data/Basedatos.py
+++ data/Basedatos.py
@@ -2,4 +2,15 @@ import sqlite3
 
 
 class BaseDatos:
-    conexion_bbdd=sqlite3.connect("bbdd_practica")
+
+    conexion_bbdd = sqlite3.connect("bbdd_practica")
+    cursor_bbdd = conexion_bbdd.cursor()
+
+    def __init__(self):
+        self.cursor_bbdd.execute("""CREATE TABLE DATOS_FORMULARIO (
+        USER_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+        NOMBRE VARCHAR(10)
+        APELLIDO VARCHAR(30),
+        PASSWORD VARCHAR (10),
+        COMENTARIOS VARCHAR(50)
+        )""")
--- gui/Gui.py
+++ gui/Gui.py
@@ -1,25 +1,35 @@
 from tkinter import *
 from tkinter.ttk import *
 
+
 class Gui(Frame):
 
     def widget_formulario(self):
+
+        self.texto_ID = Label(self, text="ID: ")
+        self.texto_ID.grid(column=1, row=1)
+        self.entrada_ID = Entry(self)
+        self.entrada_ID.grid(column=2, row=1)
+
         self.texto_nombre = Label(self, text="Nombre: ")
-        self.texto_nombre.grid(column=1, row=1)
+        self.texto_nombre.grid(column=1, row=2)
         self.entrada_nombre = Entry(self)
-        self.entrada_nombre.grid(column=2, row=1)
+        self.entrada_nombre.grid(column=2, row=2)
+
         self.texto_apellido = Label(self, text="Apellido: ")
-        self.texto_apellido.grid(column=1, row=2)
+        self.texto_apellido.grid(column=1, row=3)
         self.entrada_apellido = Entry(self)
-        self.entrada_apellido.grid(column=2, row=2)
+        self.entrada_apellido.grid(column=2, row=3)
+
         self.texto_password = Label(self, text="Password: ")
-        self.texto_password.grid(column=1, row=3)
+        self.texto_password.grid(column=1, row=4)
         self.entrada_password = Entry(self)
-        self.entrada_password.grid(column=2, row=3)
+        self.entrada_password.grid(column=2, row=4)
+
         self.texto_comentarios = Label(self, text="Comentarios: ")
-        self.texto_comentarios.grid(column=1, row=4)
+        self.texto_comentarios.grid(column=1, row=5)
         self.entrada_comentarios = Entry(self)
-        self.entrada_comentarios.grid(column=2, row=4)
+        self.entrada_comentarios.grid(column=2, row=5)
 
     def __init__(self, master=None):
         super().__init__(master)
