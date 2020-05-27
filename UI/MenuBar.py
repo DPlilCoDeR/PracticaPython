09a96b1690dfe99f001329fd825c5d973898bca1
doblepapi <jasonbnick@gmial.com>
2020-05-27 14:29:10 +0200

    Add menu in MenuBar

--- UI/MenuBar.py
+++ UI/MenuBar.py
@@ -1,5 +1,5 @@
 from tkinter import *
-
+from data.Basedatos import *
 
 class MenuBar(Menu):
 
@@ -7,4 +7,16 @@ class MenuBar(Menu):
         Menu.__init__(self, parent)
         file_menu = Menu(self, tearoff=False)
         self.add_cascade(label="File", underline=0, menu=file_menu)
-        file_menu.add_command(label="Exit", underline=1, command=self.quit)
+        file_menu.add_cascade(label="BBDD", underline=1, command=BaseDatos)
+        file_menu.add_command(label="Exit", command=self.quit)
+
+        crud_menu = Menu(self, tearoff=False)
+        self.add_cascade(label="Crud", underline=0, menu=crud_menu)
+        crud_menu.add_cascade(label="Save")
+        crud_menu.add_cascade(label="Read")
+        crud_menu.add_cascade(label="Update")
+        crud_menu.add_cascade(label="Delete")
+
+        ayuda_menu = Menu(self, tearoff=False)
+        self.add_cascade(label="Ayuda", underline=0, menu=ayuda_menu)
+        ayuda_menu.add_cascade(label="Saber mas...")
