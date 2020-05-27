1d5498a39f9aa7fb9e0b417cd4422c766975662b
doblepapi <jasonbnick@gmial.com>
2020-05-27 17:50:55 +0200

    Can create a table with menu bar

--- UI/MenuBar.py
+++ UI/MenuBar.py
@@ -5,9 +5,18 @@ class MenuBar(Menu):
 
     def __init__(self, parent):
         Menu.__init__(self, parent)
+
+        self.db = BaseDatos()
+
         file_menu = Menu(self, tearoff=False)
         self.add_cascade(label="File", underline=0, menu=file_menu)
-        file_menu.add_cascade(label="BBDD", underline=1, command=BaseDatos)
+        file_menu.add_cascade(label="Crear tabla", underline=1, command=lambda: self.db.execute("""CREATE TABLE DATOS_USUARIO(
+                                                                                         USER_ID INTEGER PRIMARY KEY
+                                                                                         AUTOINCREMENT,
+                                                                                         NOMBRE VARCHAR(20),
+                                                                                         APELLIDO VARCHAR(20), 
+                                                                                         PASSWORD VARCHAR(10),
+                                                                                         COMENTARIOS VARCHAR (80))"""))
         file_menu.add_command(label="Exit", command=self.quit)
 
         crud_menu = Menu(self, tearoff=False)
