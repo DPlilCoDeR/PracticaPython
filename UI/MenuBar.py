fcfadc9b246755ccfc35252e28564fa11461eec6
doblepapi <jasonbnick@gmial.com>
2020-06-01 15:59:06 +0200

    changed method of menus

--- UI/MenuBar.py
+++ UI/MenuBar.py
@@ -9,12 +9,16 @@ class MenuBar(Menu):
         self.db = BaseDatos()
 
         file_menu = Menu(self, tearoff=False)
-        self.add_cascade(label="File", underline=0, menu=file_menu)
-        file_menu.add_cascade(label="Crear tabla", underline=1, command=lambda: self.db.execute("""CREATE TABLE 
+        self.add_cascade(label="BBDD", underline=0, menu=file_menu)
+        file_menu.add_command(label="Crear tabla", underline=1, command=lambda: self.db.execute("""CREATE TABLE 
         DATOS_USUARIO( USER_ID INTEGER PRIMARY KEY AUTOINCREMENT, NOMBRE VARCHAR(20), APELLIDO VARCHAR(20), 
         PASSWORD VARCHAR(10), COMENTARIOS VARCHAR (80))"""))
         file_menu.add_command(label="Exit", command=self.quit)
 
+        borrar_menu = Menu(self, tearoff = False)
+        self.add_cascade(label="Borrar", menu=borrar_menu)
+        borrar_menu.add_command(label="Borrar Campos")
+
         ayuda_menu = Menu(self, tearoff=False)
         self.add_cascade(label="Ayuda", underline=0, menu=ayuda_menu)
-        ayuda_menu.add_cascade(label="Saber mas...")
+        ayuda_menu.add_command(label="Saber mas...")
