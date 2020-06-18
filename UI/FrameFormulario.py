11f9bc9a70592a7fe3662cc6fc8653f00c042557
doblepapi <jasonbnick@gmial.com>
2020-06-18 10:56:12 +0200

    add buttons here

--- UI/FrameFormulario.py
+++ UI/FrameFormulario.py
@@ -9,13 +9,13 @@ class FrameFormulario(Frame):
         super().__init__(master)
         self.controller = controller
         self.widget_formulario()
-        self.widget_crud = FrameCrud(self.controller)
+        self.buttons_crud()
         self.pack()
 
     def widget_formulario(self):
         self.texto_ID = Label(self, text="ID: ")
         self.texto_ID.grid(column=0, row=1)
-        self.id_variable = IntVar()
+        self.id_variable = StringVar()
         self.entrada_ID = Entry(self, textvariable=self.id_variable)
         self.entrada_ID.grid(column=1, row=1, columnspan=2)
 
@@ -42,3 +42,16 @@ class FrameFormulario(Frame):
         self.texto_comentarios.grid(column=0, row=5)
         self.entrada_comentarios = Text(self, width=20, height=10)
         self.entrada_comentarios.grid(column=1, row=5, columnspan=2)
+
+    def buttons_crud(self):
+        self.boton_save = Button(self, text="Save", command=lambda: self.controller.insert_item(self.nombre_variable.get(), self.apellido_variable.get(), self.password_variable.get()))
+        self.boton_save.grid(column=0, row=0)
+
+        self.boton_read = Button(self, text="Read", command=lambda: self.controller.show_item(self.id_variable.get()))
+        self.boton_read.grid(column=1, row=0)
+
+        self.boton_update = Button(self, text="Update", command=lambda: self.controller.update_item())
+        self.boton_update.grid(column=2, row=0)
+
+        self.boton_delete = Button(self, text="Delete", command=lambda: self.controller.delete_item())
+        self.boton_delete.grid(column=3, row=0)
