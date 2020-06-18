7bd06c8a608ad3490400472ae7b2fa4c10a23404
doblepapi <jasonbnick@gmial.com>
2020-06-18 10:55:25 +0200

    change in show_item

--- app/Controller.py
+++ app/Controller.py
@@ -10,7 +10,7 @@ class Controller:
     def __init__(self):
         self.root = Tk()
         self.model = Model()
-        self.view = FrameFormulario(self)
+        self.view = FrameFormulario(self, self.root)
         self.menu = MenuBar(self.root, self)
 
         self.user_id = self.view.id_variable.get()
@@ -21,49 +21,30 @@ class Controller:
         self.root.config(menu=self.menu)
         self.root.mainloop()
 
-    def borrar_campos(self):
+    def delete_form(self):
         self.view.id_variable.set("")
         self.view.nombre_variable.set("")
         self.view.apellido_variable.set("")
         self.view.password_variable.set("")
 
     def show_item(self, user_id):
-
+        print(user_id)
         user = self.model.read_item(user_id)
-        self.view.nombre_variable.set(user)
+        for dato in user:
+            self.view.nombre_variable.set(dato[1])
+            self.view.apellido_variable.set(dato[2])
+            self.view.password_variable.set(dato[3])
+
 
     def insert_item(self, name, last_name, password, comments=None):
-        item_type = self.model.item_type
-        try:
-            self.model.create_item(name, last_name, password, comments)
-        except mvc_exc.ItemAlreadyStored as e:
-            self.view.display_item_already_stored_error(name, item_type, e)
+        self.model.create_item(name, last_name, password, comments)
+        self.delete_form()
 
     def update_item(self, name, price, quantity):
-        assert price > 0, 'price must be greater than 0'
-        assert quantity >= 0, 'quantity must be greater than or equal to 0'
-        item_type = self.model.item_type
+        self.model.update_item()
 
-        try:
-            older = self.model.read_item(name)
-            self.model.update_item(name, price, quantity)
-            self.view.display_item_updated(
-                name, older['price'], older['quantity'], price, quantity)
-        except mvc_exc.ItemNotStored as e:
-            self.view.display_item_not_yet_stored_error(name, item_type, e)
-            # if the item is not yet stored and we performed an update, we have
-            # 2 options: do nothing or call insert_item to add it.
-            # self.insert_item(name, price, quantity)
-
-    def update_item_type(self, new_item_type):
-        old_item_type = self.model.item_type
-        self.model.item_type = new_item_type
-        self.view.display_change_item_type(old_item_type, new_item_type)
+    def update_item_type(self, user_id):
+        pass
 
     def delete_item(self, name):
-        item_type = self.model.item_type
-        try:
-            self.model.delete_item(name)
-            self.view.display_item_deletion(name)
-        except mvc_exc.ItemNotStored as e:
-            self.view.display_item_not_yet_stored_error(name, item_type, e)
+        pass
